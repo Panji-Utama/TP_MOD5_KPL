370697b685918b16a336e68f20501b31100c85be
FIRMAN PANJI <fpu2468@gmail.com>
2022-03-25 08:57:14 +0700

    menambahkan classSayaTubeVideo

--- tpmodul5_1302200089/tpmodul5_1302200089/Program.cs
+++ tpmodul5_1302200089/tpmodul5_1302200089/Program.cs
@@ -10,6 +10,12 @@ namespace tpmodul5_1302200089
     {
         static void Main(string[] args)
         {
+            Console.Write("Masukkan nama praktikan: ");
+            string namaPrak = Console.ReadLine();
+
+            SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract – [" + namaPrak + "]");
+            //Console.WriteLine(sayaTubeVideo.getTitle());
+            sayaTubeVideo.PrintVideoDetails();
         }
     }
 }
---
+++ tpmodul5_1302200089/tpmodul5_1302200089/SayaTubeVideo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302200089
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        //public SayaTubeVideo() { };
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random random = new Random();
+            id = random.Next(0, 100000);
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int angka)
+        {
+            playCount = playCount + angka;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("vidID \t: " + id);
+            Console.WriteLine("vidTitle \t: " + title);
+            Console.WriteLine("vidPlayCount \t: " + playCount);
+        }
+
+        //public string getTitle()
+        //{
+        //    return title;
+        //}
+    }
+}
